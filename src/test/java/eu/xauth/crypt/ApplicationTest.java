7e787c7820933e13a8c15b17b4dcd7abbbc24e8a
Bence Demeter <bdemeter@volvocars.com>
2024-05-12 19:55:27 +0200

    add spring boot webflux basics

---
+++ src/test/java/eu/xauth/crypt/ApplicationTest.java
@@ -0,0 +1,19 @@
+package eu.xauth.crypt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class ApplicationTest {
+
+  @Autowired private ApplicationContext context;
+
+  @Test
+  void contextLoads() {
+    assertThat(context).isNotNull();
+  }
+}
